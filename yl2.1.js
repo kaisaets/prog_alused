d859dbd06b48f7c8ca64d7cbbda875119e430247
Kaisa Ets <kaisa.ets@voco.ee>
2024-09-27 09:24:44 +0300

    Ylesanne 2.1

---
+++ yl2.1.js
@@ -0,0 +1,13 @@
+const readline = require('node:readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+rl.question('Sisestage õhutemperatuur: ', temperatuur =>{
+    if(temperatuur < 4.0) {
+        console.log('On jäätumise oht')
+    } else {
+        console.log('Ei ole jäätumise ohtu')
+    } 
+    rl.close()
+}
